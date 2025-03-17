8012c4d82109f1f654981fb4082e1c65c277bfe2
Bharaths73 <130239394+Bharaths73@users.noreply.github.com>
2025-03-17 19:55:05 +0530

    added chat to dashboard

--- frontend/src/Pages/Dashboard.jsx
+++ frontend/src/Pages/Dashboard.jsx
@@ -1,22 +1,66 @@
 import DashboardCard from '@/components/common/DashboardCard'
 import { getDashboardDetailsBackend } from '@/Services/operations/Dashboard'
 import React, { useEffect, useState } from 'react'
-import { useSelector } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
+import { Bar, BarChart, CartesianGrid, LabelList, XAxis, YAxis } from "recharts"
+import {
+  ChartContainer,
+  ChartTooltip,
+  ChartTooltipContent,
+} from "@/components/ui/chart"
+import { getAllOrders } from '@/Services/operations/OrdersApi'
 
 function Dashboard() {
   const [details,setDetails]=useState({})
   const token=useSelector((state)=>state.auth.token)
+  const {orders}=useSelector((state)=>state.orders)
+  const dispatch=useDispatch()
+
+  const chartData = [];
+
+  orders.forEach((order) => {
+    const existing = chartData.find((item) => item.status === order.status)
+    if (existing) {
+      existing.value++
+    } else {
+      chartData.push({ status: order.status, value: 1 })
+    }
+  })
+
+  const chartConfig = {
+    delivered: {
+      label: "Delivered",
+      color: "hsl(var(--chart-1))",
+    },
+    cancelled: {
+      label: "Cancelled",
+      color: "hsl(var(--chart-2))",
+    },
+    shipped: {
+      label: "Shipped",
+      color: "hsl(var(--chart-3))",
+    },
+    placed: {
+      label: "Placed",
+      color: "hsl(var(--chart-4))",
+    },
+    label: {
+      color: "hsl(var(--background))",
+    },
+  }
 
   const getDashBoardDetails=async()=>{
     const result=await getDashboardDetailsBackend(token)
     if(result){
        setDetails(result)
     }
+    await getAllOrders(dispatch,token)
   }
 
   useEffect(()=>{
     getDashBoardDetails()
   },[])
+
   return (
     <div className="w-full min-h-screen mt-20">
       <div className="w-11/12 mx-auto mt-5 flex flex-col gap-y-5">
@@ -28,6 +72,56 @@ function Dashboard() {
                 ))
            }
          </div>
+
+         <p className='mt-14 mb-2 text-2xl font-semibold'>Orders Analytics <span>📈</span></p>
+         <ChartContainer config={chartConfig} className="h-60 w-full ">
+         <BarChart
+            accessibilityLayer
+            data={chartData}
+            layout="vertical"
+            margin={{
+              right: 16,
+            }}
+            className=''
+          >
+            <CartesianGrid horizontal={false} />
+            <YAxis
+              dataKey="status"
+              type="category"
+              tickLine={false}
+              tickMargin={10}
+              axisLine={false}
+              tickFormatter={(value) => value.slice(0, 3)}
+              hide
+            />
+            <XAxis dataKey="value" type="number" hide />
+            <ChartTooltip
+              cursor={false}
+              content={<ChartTooltipContent indicator="line" />}
+            />
+            <Bar
+              dataKey="value"
+              layout="vertical"
+              fill="var(--color-desktop)"
+              radius={4}
+            >
+              <LabelList
+                dataKey="status"
+                position="insideLeft"
+                offset={8}
+                className="fill-white"
+                fontSize={15}
+              />
+              <LabelList
+                dataKey="value"
+                position="right"
+                offset={8}
+                className="fill-foreground font-bold"
+                fontSize={12}
+              />
+            </Bar>
+          </BarChart>
+          </ChartContainer>
       </div>
     </div>
   )
